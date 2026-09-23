d264441d8a84a150f57bc38f0906afb811171e70
0Nut0 <2302308@uad.ac.uk>
2026-09-23 19:32:55 +0100

    looked into css stuff and classes

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import linus from './assets/Linus.png';
+
 import './App.css';
+
+export function Button()
+{
+  return <button>Im A Button</button>
+}
+
+export function Profile()
+{
+  return <img className = "ProfilePicture" src = {linus} alt = "linus" width = {300} height = {400}></img>;
+}  
+
+
+export function AboutMe()
+{
+  return(
+  <>
+  <h1 className = "MainHeader"> About Me</h1>
+    <p>Hello There. i am liam</p>
+  </>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Profile/>      
+      <section>
+          <h1 >Liam Collins</h1>
+            <Button/>
+      </section>
+
+
+    <section>
+      <AboutMe/>
+    </section>
+
     </div>
   );
 }
