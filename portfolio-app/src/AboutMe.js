938b8907ce05b3d0a16b78642905dbfc7bf01711
Dev-Nut-creator <liamc132005@hotmail.com>
2026-09-24 21:48:05 +0100

    Site layout almost completed. Working on project section layout then can add project content in

---
+++ portfolio-app/src/AboutMe.js
@@ -0,0 +1,30 @@
+
+import './AboutMe.css'
+
+const clicked = false;
+
+export function OpenAboutMe()
+{
+    <div className =  "square">
+
+    </div>
+    
+}
+
+export default function AboutMeButtonClicked()
+{
+    // clicked = !clicked;
+
+    // if(clicked)
+    // {
+    //     Navigate('/aboutme')
+    //     return <OpenAboutMe/>;
+    // }
+    // else
+    // {
+    //     Navigate('/app')
+    //     return;
+    // }
+
+}
+
--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,45 +1,127 @@
 // import logo from './logo.svg';
 import linus from './assets/Linus.png';
+import homer from './assets/homer.gif';
+import egg from './assets/egg.png'
+
 
 import './App.css';
 
-export function Button()
+import { useState  } from 'react';
+
+export function Button({name, func, className})
 {
-  return <button>Im A Button</button>
+  return <button className={className} onClick= {func}>{name}</button>
 }
 
-export function Profile()
+export function ImageButton({name,func,className,imageClassName,image,alt})
 {
-  return <img className = "ProfilePicture" src = {linus} alt = "linus" width = {300} height = {400}></img>;
-}  
-
-
-export function AboutMe()
-{
-  return(
-  <>
-  <h1 className = "MainHeader"> About Me</h1>
-    <p>Hello There. i am liam</p>
-  </>
+  return( 
+    <div>
+    <button className={className} onClick= {func}>
+      <img className={imageClassName} src = {image} alt= {alt} ></img>
+      <p className='ProjectButtonText'>{name}</p>
+    </button>
+    </div>
   );
 }
 
 
-function App() {
-  return (
-    <div className="App">
-        <Profile/>      
-      <section>
-          <h1 >Liam Collins</h1>
-            <Button/>
+export function Image({className,resource,altName})
+{
+  return <img className={className} src = {resource} alt = {altName}/>;
+}  
+
+
+
+export function AboutMe()
+{
+  //write about me page here
+  return(
+    <div className='AboutMeSection'>
+        {/* create the container for the about me section*/}
+        <section className ='SquareContainer'>
+
+
+        </section>
+    </div>
+  );
+
+}
+
+
+
+export function NuttyEngine()
+{
+  return(
+    <div className='NuttyEngineSection'>
+
+      <section class = 'SquareContainer' style ={{position: 'absolute' ,left: '200px' ,bottom : '200px' }}>
+
+
       </section>
 
+    </div>
 
-    <section>
-      <AboutMe/>
-    </section>
+
+  );
+
+}
+
+export function DES315()
+{
+  return(
+    <div className='DES315Section'>
+        <section className='SquareContainer' style ={{'--position': 'absolute'}} >
+
+
+        </section>
 
     </div>
+  );
+
+}
+
+
+
+function App() {
+  // //make various states for each element that can be conditionally rendered
+  const [aboutMeOpen, SetAboutMeOpen] = useState(false);
+  const [EngineProjectOpen, SetEngineProjectOpen] = useState(false);
+  const [DES315ProjectOpen, SetDES315ProjectOpen] = useState(false);
+
+
+  return (
+    <div>
+        <section className='ProjectGifSection'>
+          <Image className= 'ProjectGifContainer' resource={homer} altName='homer'/>
+        </section>
+        <section className='ProfileSection'>
+          <Image className = 'ProfileContainer' resource = {linus} altName = 'Linus'/>      
+        </section>
+        {/* //render about me page if about me button clicked  */}
+        <section className='AboutMeButtonContainer'>
+            <Button className =  "AboutMeButton" name = "About Me" func = {() => SetAboutMeOpen(!aboutMeOpen)}/> 
+            {aboutMeOpen && <AboutMe/>}
+        </section>
+
+        <section className='ProjectSection'>
+          {/* create buttons that hold a gif to each project and each of their own sections for showing text*/}
+          <ImageButton className='NuttyEngineProject' name = 'NuttyEngine' func = {() =>SetEngineProjectOpen(!EngineProjectOpen)} image={egg} imageClassName='ProjectButtonImage' alt = 'NuttyEngine'/>
+            {EngineProjectOpen && <NuttyEngine/>}
+          
+          <ImageButton className='DES315Project' name = 'DES315' func = {() =>SetDES315ProjectOpen(!DES315ProjectOpen)} image={egg} imageClassName='ProjectButtonImage' alt = 'DES315' />
+            {DES315ProjectOpen && <DES315/>}
+        </section>
+
+
+
+
+    </div>
+
+
+
+  
+
   );
 }
 
